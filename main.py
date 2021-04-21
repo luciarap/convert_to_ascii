ed6c4348d93d2fa69d49774af6ad8e1158e17356
Lucia Rapánová <lucia.rapanova@gmail.com>
2021-04-21 17:35:53 +0200

    pridavne mena, hesla

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import unicodedata
 
-
 if __name__ == '__main__':
 
     with open("slovak_dict_ascii.txt", 'w') as output:
@@ -13,3 +12,14 @@ if __name__ == '__main__':
 
     output.close()
     sk_dict.close()
+
+    prid_mena = []
+    with open('prid_mena', 'r', encoding='utf8') as f:
+        for line in f:
+            prid_mena.append(line.split(None, 1)[0])
+    with open("prid_mena_ascii.txt", 'w') as output:
+        for p in prid_mena:
+            p_low = p.lower()
+            output.write(''.join(
+                (c for c in unicodedata.normalize('NFD', p_low) if unicodedata.category(c) != 'Mn')))
+            output.write("\n")
