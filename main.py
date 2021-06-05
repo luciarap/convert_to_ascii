9bb84c41d4e5179afbad999d76a01bee244782d9
Lucia Rapánová <lucia.rapanova@gmail.com>
2021-06-05 14:33:06 +0200

    added example files

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import unicodedata
 
-if __name__ == '__main__':
 
-    with open("slovak_dict_ascii.txt", 'w') as output:
-        sk_dict = open("sk.dic", "r", encoding='utf8')
+# removes special characters from .dic file
+# see example file
+def dic_file_ascii(outputfile, inputfile):
+    with open(outputfile, 'w') as output:
+        sk_dict = open(inputfile, "r", encoding='utf8')
         f_lines = sk_dict.readlines()
         for line in f_lines:
             line_normalized = line.lower().replace(" ", "")
@@ -13,13 +15,27 @@ if __name__ == '__main__':
     output.close()
     sk_dict.close()
 
+
+# used to extract the first word (adjective) and removes special characters
+# see example file
+def first_adjective_ascii(outputfile, inputfile):
     prid_mena = []
-    with open('prid_mena', 'r', encoding='utf8') as f:
+    with open(inputfile, 'r', encoding='utf8') as f:
         for line in f:
             prid_mena.append(line.split(None, 1)[0])
-    with open("prid_mena_ascii.txt", 'w') as output:
+    with open(outputfile, 'w') as output:
         for p in prid_mena:
             p_low = p.lower()
             output.write(''.join(
                 (c for c in unicodedata.normalize('NFD', p_low) if unicodedata.category(c) != 'Mn')))
             output.write("\n")
+
+    f.close()
+    output.close()
+
+
+if __name__ == '__main__':
+    dic_file_ascii("sk_dic_outputfile_example.txt", "sk_dic_inputfile_example.dic")
+    first_adjective_ascii("adjectives_outfile_example.txt", "adjectives_inputfile_example.txt")
+    print("---")
+
